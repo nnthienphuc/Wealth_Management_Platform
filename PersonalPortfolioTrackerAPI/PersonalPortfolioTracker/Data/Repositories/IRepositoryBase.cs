2e40805dcbc7afae9170452d94ff1b47bc27ebb5
Nguyen Ngoc Thien Phuc <nnthienphuc@gmail.com>
2026-04-09 17:55:53 +0700

    Create RepositoryBase for for convenience in coding

---
+++ PersonalPortfolioTrackerAPI/PersonalPortfolioTracker/Data/Repositories/IRepositoryBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace PersonalPortfolioTracker.Data.Repositories
+{
+    public interface IRepositoryBase<T> where T : class
+    {
+        IQueryable<T> FindAll(bool trackChanges = false);
+        IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges = false);
+        void Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
