3cec604bb2471c51a58a0121988e8d9eac71cef8
Nguyen Ngoc Thien Phuc <nnthienphuc@gmail.com>
2026-05-05 13:23:28 +0700

    chore: temporarily disable Transaction methods to unblock HoldingsService development

--- PersonalPortfolioTrackerAPI/PersonalPortfolioTracker/Services/TransactionService/ITransactionService.cs
+++ PersonalPortfolioTrackerAPI/PersonalPortfolioTracker/Services/TransactionService/ITransactionService.cs
@@ -4,7 +4,7 @@ namespace PersonalPortfolioTracker.Services.TransactionService
 {
     public interface ITransactionService
     {
-        Task<IEnumerable<AccountTypeResponse>> GetInvestAccount();
-        Task<PagedResponse<TransactionResponse>> FindByConditionAsync(Guid accountID, string? tickerSymbol, string transactionType, DateOnly? tradeDate);
+        //Task<IEnumerable<AccountTypeResponse>> GetInvestAccount();
+        //Task<PagedResponse<TransactionResponse>> FindByConditionAsync(Guid accountID, string? tickerSymbol, string transactionType, DateOnly? tradeDate);
     }
 }
