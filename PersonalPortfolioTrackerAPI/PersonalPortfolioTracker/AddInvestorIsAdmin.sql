0443daf46c53d904bfa86d7c45bd6759cb5f4ac5
Nguyen Ngoc Thien Phuc <nnthienphuc@gmail.com>
2026-08-19 12:22:53 +0700

    feat: add admin authorization

---
+++ PersonalPortfolioTrackerAPI/PersonalPortfolioTracker/AddInvestorIsAdmin.sql
@@ -0,0 +1,13 @@
+﻿BEGIN TRANSACTION;
+GO
+
+ALTER TABLE [Investors] ADD [IsAdmin] bit NOT NULL DEFAULT CAST(0 AS bit);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20260819034312_AddInvestorIsAdmin', N'8.0.25');
+GO
+
+COMMIT;
+GO
+
