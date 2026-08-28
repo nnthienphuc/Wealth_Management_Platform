b3d8457f951a07b2e581be09f665496e69b233d1
Nguyen Ngoc Thien Phuc <nnthienphuc@gmail.com>
2026-08-28 11:03:15 +0700

    feat: add holding stop loss

---
+++ PersonalPortfolioTrackerAPI/PersonalPortfolioTracker/AddHoldingStopLoss.sql
@@ -0,0 +1,13 @@
+﻿BEGIN TRANSACTION;
+GO
+
+ALTER TABLE [Holdings] ADD [StopLoss] decimal(28,8) NULL;
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20260828031407_AddHoldingStopLoss', N'8.0.25');
+GO
+
+COMMIT;
+GO
+
